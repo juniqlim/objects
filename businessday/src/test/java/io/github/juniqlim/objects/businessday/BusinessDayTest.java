106b1d02188aa81de3ec27b3d163b2d1c3ecfc3c
juniq <juniqlim@gmail.com>
2022-10-03 21:00:25 +0900

    test: 코드 단순화

--- businessday/src/test/java/io/github/juniqlim/objects/businessday/BusinessDayTest.java
+++ businessday/src/test/java/io/github/juniqlim/objects/businessday/BusinessDayTest.java
@@ -2,27 +2,26 @@ package io.github.juniqlim.objects.businessday;
 
 import java.io.IOException;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BusinessDayTest {
     @Test
-    public void test() throws IOException {
+    public void 입력한_날짜_기준_이전_영업일_조회() throws IOException {
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("20220926", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
-            LocalDate.parse("20220923", DateTimeFormatter.BASIC_ISO_DATE));
+            new BusinessDay(LocalDate.parse("2022-09-26")).previousDate(),
+            LocalDate.parse("2022-09-23"));
 
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("20221004", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
-            LocalDate.parse("20220930", DateTimeFormatter.BASIC_ISO_DATE));
+            new BusinessDay(LocalDate.parse("2022-10-04")).previousDate(),
+            LocalDate.parse("2022-09-30"));
 
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("20220913", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
-            LocalDate.parse("20220908", DateTimeFormatter.BASIC_ISO_DATE));
+            new BusinessDay(LocalDate.parse("2022-09-13")).previousDate(),
+            LocalDate.parse("2022-09-08"));
 
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("20220930", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
-            LocalDate.parse("20220929", DateTimeFormatter.BASIC_ISO_DATE));
+            new BusinessDay(LocalDate.parse("2022-09-30")).previousDate(),
+            LocalDate.parse("2022-09-29"));
     }
 }
