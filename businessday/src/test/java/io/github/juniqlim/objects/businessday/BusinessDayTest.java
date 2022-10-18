0c6462df476465d3ec001dc7a26c9a7087bdc43c
juniq <juniqlim@gmail.com>
2022-10-18 10:23:48 +0900

    feat(영업일): 테스트 수정

--- businessday/src/test/java/io/github/juniqlim/objects/businessday/BusinessDayTest.java
+++ businessday/src/test/java/io/github/juniqlim/objects/businessday/BusinessDayTest.java
@@ -2,26 +2,27 @@ package io.github.juniqlim.objects.businessday;
 
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class BusinessDayTest {
     @Test
-    public void 입력한_날짜_기준_이전_영업일_조회() throws IOException {
+    public void test() throws IOException {
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("2022-09-26")).previousDate(),
-            LocalDate.parse("2022-09-23"));
+            new BusinessDay(LocalDate.parse("20220926", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
+            LocalDate.parse("20220923", DateTimeFormatter.BASIC_ISO_DATE));
 
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("2022-10-04")).previousDate(),
-            LocalDate.parse("2022-09-30"));
+            new BusinessDay(LocalDate.parse("20221004", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
+            LocalDate.parse("20220930", DateTimeFormatter.BASIC_ISO_DATE));
 
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("2022-09-13")).previousDate(),
-            LocalDate.parse("2022-09-08"));
+            new BusinessDay(LocalDate.parse("20220913", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
+            LocalDate.parse("20220908", DateTimeFormatter.BASIC_ISO_DATE));
 
         Assertions.assertEquals(
-            new BusinessDay(LocalDate.parse("2022-09-30")).previousDate(),
-            LocalDate.parse("2022-09-29"));
+            new BusinessDay(LocalDate.parse("20220930", DateTimeFormatter.BASIC_ISO_DATE)).previousDate(),
+            LocalDate.parse("20220929", DateTimeFormatter.BASIC_ISO_DATE));
     }
 }
