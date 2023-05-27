bfbd5c7ce27483dbbcfda59a62db70e82e53e509
juniq <juniqlim@gmail.com>
2023-05-27 22:06:51 +0900

    feat(jwt): 파라미터로 claims를 추가하도록 변경

--- jwt/src/main/java/io/github/juniqlim/object/jwt/Jwt.java
+++ jwt/src/main/java/io/github/juniqlim/object/jwt/Jwt.java
@@ -7,6 +7,7 @@ import org.jose4j.jwt.NumericDate;
 import org.jose4j.lang.JoseException;
 
 import java.security.PrivateKey;
+import java.util.Map;
 
 public interface Jwt {
     String token();
@@ -19,11 +20,39 @@ public interface Jwt {
         }
 
         public String token() {
-            JwtClaims claims = new JwtClaims();
-            claims.setExpirationTime(NumericDate.fromSeconds(60*60*24*365));
-            claims.setGeneratedJwtId();
-            claims.setIssuedAtToNow();
+            return jwsString(claims());
+        }
 
+        public String token(Request request) {
+            return jwsString(claims(request));
+        }
+
+        public String token(Map<String, Object> payload) {
+            return jwsString(claims(payload));
+        }
+
+        private JwtClaims claims(Map<String, Object> payload) {
+            JwtClaims claims = new JwtClaims();
+            payload.forEach(claims::setClaim);
+            return claims;
+        }
+
+        private static JwtClaims claims(Request request) {
+            JwtClaims claims = new JwtClaims();
+            claims.setIssuedAtToNow();
+            claims.setExpirationTime(NumericDate.fromSeconds(request.expirationSeconds));
+            request.payload.forEach(claims::setClaim);
+            return claims;
+        }
+
+        private static JwtClaims claims() {
+            JwtClaims claims = new JwtClaims();
+            claims.setIssuedAtToNow();
+            claims.setExpirationTime(NumericDate.fromSeconds(60*60*24*365));
+            return claims;
+        }
+
+        private String jwsString(JwtClaims claims) {
             JsonWebSignature jws = new JsonWebSignature();
             jws.setKey(privateKey);
             jws.setHeader("typ", "JWT");
@@ -35,6 +64,16 @@ public interface Jwt {
             } catch (JoseException e) {
                 e.printStackTrace();
                 throw new RuntimeException(e);
+            }
+        }
+
+        public static class Request {
+            private final long expirationSeconds;
+            private final Map<String, Object> payload;
+
+            public Request(long expirationSeconds, Map<String, Object> payload) {
+                this.expirationSeconds = expirationSeconds;
+                this.payload = payload;
             }
         }
     }
